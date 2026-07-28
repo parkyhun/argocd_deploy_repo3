89282ccdde00b6886fb617b41d816cf3881de5ec
parkyhun <yy9976com@gamil.com>
2026-07-28 10:10:57 +0900

    1 add

---
+++ hello/apps/fortune/src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/fortune")
+def get_fortune():
+    return {
+        "service": "fortune", 
+        #"message": "동쪽으로 가명 귀인을 만나요"
+        "message": "하이닉스 300만원 다시 갈거야!삼성전자는 40만원, 두산에너빌 20만원, 현대차는 70만원"
+    }
---
+++ hello/apps/greet/src/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/greet")
+def get_fortune():
+    return {
+        "service": "greet", 
+        "message": "안녕하세요, 즐거운 하루, 즐거운 한 주"
+    }
