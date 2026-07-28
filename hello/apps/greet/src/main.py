7e5fa2ee67745af0bdd7f146fe2711496a053944
parkyhun <yy9976com@gamil.com>
2026-07-28 11:34:26 +0900

    test1234

--- hello/apps/greet/src/main.py
+++ hello/apps/greet/src/main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 def get_fortune():
     return {
         "service": "greet", 
-        "message": "안녕하세요, 즐거운 하루, 즐거운 한 주, 즐거운 한 달"
+        "message": "안녕하세요, 즐거운 하루, 즐거운 한 주, 즐거운 한 달 될거에요"
     }
